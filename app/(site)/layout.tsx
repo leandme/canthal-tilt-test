4755b9ea2143503ca2f0f1c9257fd683fdb6d8cd
Matt Phelps <matt@leandme.com>
2026-05-10 09:38:55 +0200

    Add Google Analytics tag and ads.txt

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import AmplitudeInitializer from "../components/AmplitudeInitializer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -10,8 +11,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0DEQLQNY3L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0DEQLQNY3L');
+          `}
+        </Script>
+      </head>
       <body className="bg-base-100 text-base-content">
-      <AmplitudeInitializer />
+        <AmplitudeInitializer />
         <Navbar />
         <main className="container mx-auto px-4 lg:px-8 py-8 min-h-screen">
           {children}
